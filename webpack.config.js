1e4e259b0c20271ab8569668156db2358c8a1d1d
UMIQLO <umiqlo.moe@gmail.com>
2017-12-11 15:36:35 +0800

    fine tune some dependencies, eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const path = require('path');
-const webpack = require('webpack');
-const buildPath = path.resolve(__dirname, 'build');
+const path = require('path')
+const webpack = require('webpack')
+
+const buildPath = path.resolve(__dirname, 'build')
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: buildPath,
-        filename: 'bundle.js',
-        publicPath: '/build/'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './',
-        devtool: 'eval',
-        hot: true, // Live-reload
-        inline: true,
-        port: 21314, // Port Number
-        host: 'localhost', // Change to '0.0.0.0' for external facing server
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)/,
-                use: [
-                    'babel-loader',
-                    'eslint-loader'
-                ]
-            }, {
-                test: /\.(sass|scss|css)/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "resolve-url-loader",
-                    "sass-loader?sourceMap"
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+    path: buildPath,
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './',
+    hot: true, // Live-reload
+    inline: true,
+    port: 21314, // Port Number
+    host: 'localhost' // Change to '0.0.0.0' for external facing server
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        use: [
+          'babel-loader',
+          'eslint-loader'
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    stats: {
-        // Nice colored output
-        colors: true
-    },
+      }, {
+        test: /\.(sass|scss|css)/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader',
+          'sass-loader?sourceMap'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  stats: {
+    // Nice colored output
+    colors: true
+  }
 }
 
-module.exports = config;
+module.exports = config
